2122dc875066d710b1c3d6e5af9b59e480f0ab5b
Gabriel Caceres <caceresgabriel139@gmail.com>
2025-11-11 19:27:17 -0300

    .

--- src/main/java/aed/Edr.java
+++ src/main/java/aed/Edr.java
@@ -137,26 +137,9 @@ public class Edr {
     }
 
 // -------------------------------------------------CONSULTAR DARKWEB------------------------------------------------- //
-/*
-consultarDarkWeb: 
-Se calcula la nota del examen modelo de la Darkweb O(R)
-Sacar los primeros k que no entregaron del heap O(K) y reheapifica O(K * log E)
-Por cada uno, reemplaza su examen O(1) y le asigna EL examen modelo DarkWeb O(1)
-Complejidad Total: O(K (R + log E) )
 
-*/
-
-    public void consultarDarkWeb(int k, int[] examenDW) {
-        for (int i = 0; i < k; i++){
-            reemplazarDW(i, examenDW, examenCanonico);
-        }
-    }
-
-    public void reemplazarDW(int id, int[] examenDarkWeb, ArrayList<Integer> examenCanon) {
-        for (int i = 0; i < examenCanon.size();i++){
-            
-            actualizarNota(id, i, examenDarkWeb[i]);
-        }
+    public void consultarDarkWeb(int n, int[] examenDW) {
+        throw new UnsupportedOperationException("Sin implementar");
     }
 
 // -------------------------------------------------ENTREGAR------------------------------------------------- //
@@ -178,4 +161,3 @@ Complejidad Total: O(K (R + log E) )
         throw new UnsupportedOperationException("Sin implementar");
     }
 }
-
